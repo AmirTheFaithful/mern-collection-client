a5861d515a2451350008df06d9a7fd2e06136b63
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-05 02:20:06 +0330

    Improve component typing

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM, { Root } from "react-dom/client";
 
 import App from "./App";
 
-const root = ReactDOM.createRoot(
+const root: Root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
