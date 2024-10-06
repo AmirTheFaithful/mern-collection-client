b78ddce6e59989bafa4c6d236980228b30f04e80
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-07 02:02:39 +0330

    Complete first component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,26 @@
 import { ReactElement, FC } from "react";
+import { AiFillFire } from "react-icons/ai";
 
 import "./navbar.scss";
 
 export default function Navbar(): ReactElement {
+  handlePageScroll();
+
   return (
-    <nav className="navbar">
-      a
+    <nav className="navbar hidden">
+      <Icon />
       <LinksContainer />
     </nav>
   );
 }
+
+const Icon: FC = (): ReactElement => {
+  return (
+    <a className="nav-logo" href="/">
+      <img src="https://seeklogo.com/images/M/magento-logo-3EDB101875-seeklogo.com.png" />
+    </a>
+  );
+};
 
 const LinksContainer: FC = (): ReactElement => {
   return (
@@ -18,4 +29,20 @@ const LinksContainer: FC = (): ReactElement => {
       <a href="/">Projects</a>
     </div>
   );
+};
+
+const handlePageScroll = (): void => {
+  window.addEventListener("scroll", (): void => {
+    const navbar: HTMLElement | null = document.querySelector(".navbar");
+
+    if (!navbar) return;
+
+    if (window.scrollY < 30) {
+      navbar.classList.remove("showing");
+      navbar.classList.add("hidden");
+    } else {
+      navbar.classList.remove("hidden");
+      navbar.classList.add("showing");
+    }
+  });
 };
