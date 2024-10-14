4b46626e7ab7097a9717025e2e16de9548551c4d
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-14 15:02:48 +0330

    Add SVG to the project

--- src/pages/home-page/index.tsx
+++ src/pages/home-page/index.tsx
@@ -1,5 +1,11 @@
 import { ReactElement } from "react";
 
+import { ReactElement as CircleSvg } from "../../assets/svg/circle.svg";
+
 export default (): ReactElement => {
-  return <main></main>;
+  return (
+    <main>
+      <CircleSvg />
+    </main>
+  );
 };
