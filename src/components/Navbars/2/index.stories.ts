e3d3cfebf438d9e6a8d69b71c5dc1358f199f104
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-17 19:56:30 +0330

    Install and initialize storybook usage for documentations.

---
+++ src/components/Navbars/2/index.stories.ts
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Navbar from "./";
+
+const meta: Meta<typeof Navbar> = {
+  component: Navbar,
+};
+export default meta;
+
+type Story = StoryObj<typeof Navbar>;
+
+export const Basic: Story = {};
+
+export const Primary: Story = {
+  args: {
+    primary: true,
+  },
+};
