e330e7f44413f26679536dd3f10b3247200eb139
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-19 23:20:25 +0330

    Make Navbar #2 more transparent

--- src/components/ShortPost/PostInfoCard/PicFooter.tsx
+++ src/components/ShortPost/PostInfoCard/PicFooter.tsx
@@ -1,12 +1,25 @@
-import { FC, ReactElement } from "react";
+import { FC, ReactElement, useState } from "react";
+import {
+  AiFillLike,
+  AiFillDislike,
+  AiOutlineLike,
+  AiOutlineDislike,
+} from "react-icons/ai";
 
 import "./pic-footer.scss";
 
 const PicFooter: FC = (): ReactElement => {
+  const [liked, setLiked] = useState<boolean>(false);
+
   return (
     <section className="pic__footer">
-      <button className="pic__footer__like-btn"></button>
-      <button className="pic__foter__open-btn"></button>
+      <button
+        className="pic__footer__like-btn"
+        onClick={(): void => setLiked(!liked)}
+      >
+        {liked ? <AiFillLike size={20} /> : <AiOutlineLike size={20} />}
+      </button>
+      <button className="pic__footer__open-btn">Visit Profile</button>
     </section>
   );
 };
