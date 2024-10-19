b082f8a5bba6dbc1a9dd95648b8fbf94c81fd7eb
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-19 23:30:08 +0330

    Add sound effect to the like button of the short post preview card.

--- src/components/ShortPost/PostInfoCard/PicFooter.tsx
+++ src/components/ShortPost/PostInfoCard/PicFooter.tsx
@@ -6,16 +6,22 @@ import {
   AiOutlineDislike,
 } from "react-icons/ai";
 
+import clickSound from "../../../assets/audio/click.mp3";
 import "./pic-footer.scss";
 
 const PicFooter: FC = (): ReactElement => {
   const [liked, setLiked] = useState<boolean>(false);
+  // The like button will temporarily play the famous click sound
+  const audio = new Audio(clickSound);
 
   return (
     <section className="pic__footer">
       <button
         className="pic__footer__like-btn"
-        onClick={(): void => setLiked(!liked)}
+        onClick={(): void => {
+          setLiked(!liked);
+          audio.play();
+        }}
       >
         {liked ? <AiFillLike size={20} /> : <AiOutlineLike size={20} />}
       </button>
