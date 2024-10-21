b2a84a0c2ad24a288d7d70c87526be25bb44aba1
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-21 15:26:23 +0330

    Add shadow effect to the PostInfoCard's buttons.

--- src/components/ShortPost/PostInfoCard/PicFooter.tsx
+++ src/components/ShortPost/PostInfoCard/PicFooter.tsx
@@ -6,33 +6,49 @@ import {
   AiOutlineLike,
   AiOutlineDislike,
 } from "react-icons/ai";
+import { FaCommentDots, FaRegCommentDots } from "react-icons/fa";
 
-import bellSound from "../../../assets/audio/ui-bell.mp3";
+import likeSound from "../../../assets/audio/finger-snap.mp3";
 import "./pic-footer.scss";
 
 const PicFooter: FC = (): ReactElement => {
-  const [liked, setLiked] = useState<boolean>(false);
-  // Initialise like button sound effect
-  const audio = new Audio(bellSound);
-
   // Localisation hook
   const { t } = useTranslation();
 
   return (
     <section className="pic__footer">
-      <button
-        className="pic__footer__like-btn"
-        onClick={(): void => {
-          setLiked(!liked);
-          audio.play();
-        }}
-      >
-        {liked ? <AiFillLike size={20} /> : <AiOutlineLike size={20} />}
-      </button>
+      <LikeButton />
+      <CommentsButton />
       <button className="pic__footer__open-btn">
         {t("postInfoCard.visit")}
       </button>
     </section>
+  );
+};
+
+const LikeButton: FC = (): ReactElement => {
+  const [liked, setLiked] = useState<boolean>(false);
+  // Initialise like button sound effect
+  const audio = new Audio(likeSound);
+
+  return (
+    <button
+      className="pic__footer__like-btn"
+      onClick={(): void => {
+        setLiked(!liked);
+        audio.play();
+      }}
+    >
+      {liked ? <AiFillLike size={20} /> : <AiOutlineLike size={20} />}
+    </button>
+  );
+};
+
+const CommentsButton: FC = (): ReactElement => {
+  return (
+    <button className="pic__footer__comments-btn">
+      <FaRegCommentDots size={20} />
+    </button>
   );
 };
 
