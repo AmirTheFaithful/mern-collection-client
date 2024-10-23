a099def15fbc4289bdb1a3a85377116b6cccf6cd
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-23 20:44:12 +0330

    Complete EmojiSelector component.

--- src/components/ShortPost/PostInfoCard/PicFooter.tsx
+++ src/components/ShortPost/PostInfoCard/PicFooter.tsx
@@ -1,12 +1,24 @@
-import { FC, ReactElement, useState } from "react";
+import { FC, ReactElement, useState, Dispatch, SetStateAction } from "react";
 import { useTranslation } from "react-i18next";
+import { IconType } from "react-icons";
 import {
   AiFillLike,
   AiFillDislike,
   AiOutlineLike,
   AiOutlineDislike,
 } from "react-icons/ai";
-import { FaCommentDots, FaRegCommentDots } from "react-icons/fa";
+import {
+  FaCommentDots,
+  FaRegCommentDots,
+  FaHeart,
+  FaRegHeart,
+  FaRegAngry,
+  FaAngry,
+  FaRegLaugh,
+  FaLaugh,
+  FaRegSadTear,
+  FaSadTear,
+} from "react-icons/fa";
 
 import likeSound from "../../../assets/audio/finger-snap.mp3";
 import "./pic-footer.scss";
@@ -27,7 +39,12 @@ const PicFooter: FC = (): ReactElement => {
 };
 
 const LikeButton: FC = (): ReactElement => {
-  const [liked, setLiked] = useState<boolean>(false);
+  const [reacted, setReacted] = useState<boolean>(false);
+  const [icon, setIcon] = useState<any>(
+    reacted ? <AiFillLike size={20} /> : <AiOutlineLike size={20} />
+  );
+  const [scrolled, setScrolled] = useState<boolean>(false);
+
   // Initialise like button sound effect
   const audio = new Audio(likeSound);
 
@@ -35,12 +52,62 @@ const LikeButton: FC = (): ReactElement => {
     <button
       className="pic__footer__like-btn"
       onClick={(): void => {
-        setLiked(!liked);
+        setReacted(!reacted);
         audio.play();
       }}
+      onMouseEnter={(): void => {
+        setScrolled(true);
+      }}
+      onMouseLeave={(): void => {
+        setScrolled(false);
+      }}
     >
-      {liked ? <AiFillLike size={20} /> : <AiOutlineLike size={20} />}
+      {icon}
+      <EmojiSelector state={scrolled} setIcon={setIcon} />
     </button>
+  );
+};
+
+const EmojiSelector: FC<{
+  state: boolean;
+  setIcon: Dispatch<SetStateAction<any>>;
+}> = ({ state, setIcon }): ReactElement => {
+  return (
+    <div
+      className={`emoji-selector ${state ? "scrolled" : "hidden"}`}
+      onClick={(): void => console.log("hi!")}
+    >
+      <AiFillLike
+        className="emoji-selector__icon"
+        size={20}
+        onClick={(): void => setIcon(<AiFillLike size={20} />)}
+      />
+      <AiFillDislike
+        className="emoji-selector__icon"
+        size={20}
+        onClick={(): void => setIcon(<AiFillDislike size={20} />)}
+      />
+      <FaHeart
+        className="emoji-selector__icon"
+        size={20}
+        onClick={(): void => setIcon(<FaHeart size={20} />)}
+      />
+      <FaAngry
+        className="emoji-selector__icon"
+        size={20}
+        onClick={(): void => setIcon(<FaAngry size={20} />)}
+      />
+      <FaLaugh
+        className="emoji-selector__icon"
+        size={20}
+        onClick={(): void => setIcon(<FaLaugh size={20} />)}
+      />
+      <FaSadTear
+        className="emoji-selector__icon"
+        size={20}
+        onClick={(): void => setIcon(<FaSadTear size={20} />)}
+      />
+    </div>
   );
 };
 
