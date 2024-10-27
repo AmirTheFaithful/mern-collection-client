c77c0049051861a0e8004c99ac87d703f42d0241
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-28 03:10:13 +0330

    Minor clent side types change.

--- src/@types.d.ts
+++ src/@types.d.ts
@@ -1,11 +1,12 @@
-declare interface UserInterface {
+declare interface IUser {
   username: string;
   email: string;
   avatarURL: string;
 }
 
-declare interface CommentInterface {
-  author: string; // Actually it's the ID of the user
+declare interface IComment {
+  author: IUser; // Instead, should be the ID of the user
   content: string;
+  publicationDate: Date;
   // Later, reactions list should be provided
 }
