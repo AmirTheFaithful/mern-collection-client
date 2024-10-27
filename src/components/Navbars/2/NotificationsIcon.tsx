84f8894efee9f7bfd73888ca2db746566c528a67
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-28 02:24:15 +0330

    Add NotificationsIcon component.

---
+++ src/components/Navbars/2/NotificationsIcon.tsx
@@ -0,0 +1,27 @@
+import { FC, ReactElement } from "react";
+import { FaRegBell, FaBell } from "react-icons/fa";
+
+import NotificationBadge from "../../common/NotificationBadge";
+
+import "./notifications-icon.scss";
+
+const NotificationsIcon: FC = (): ReactElement => {
+  let notifications: number[] = [1];
+
+  if (notifications.length > 0) {
+    return (
+      <i className="notifications-icon ">
+        <FaBell size={23} />
+        <NotificationBadge count={notifications.length} />
+      </i>
+    );
+  } else {
+    return (
+      <>
+        <FaRegBell className="notifications-icon" size={23} />
+      </>
+    );
+  }
+};
+
+export default NotificationsIcon;
