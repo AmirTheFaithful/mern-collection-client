9071a0e12be46a0129282109d5460df360f9c299
Amir Daneshmand <amirdanesh02@gmail.com>
2024-10-29 17:55:00 +0330

    Add state management store center.

---
+++ src/store/index.ts
@@ -0,0 +1,14 @@
+import { commentsSlice } from "./features/comments-slice";
+import { configureStore } from "@reduxjs/toolkit";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+export const store = configureStore({
+  reducer: {
+    comments: commentsSlice.reducer,
+  },
+});
+
+export const useAppDispatch: () => typeof store.dispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<
+  ReturnType<typeof store.getState>
+> = useSelector;
