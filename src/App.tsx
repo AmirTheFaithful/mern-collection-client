c1fba90140dc06825488ed75f83be2c783ac9bb1
Amir Daneshmand <amirdanesh02@gmail.com>
2024-12-08 13:45:22 +0330

    Minor code correctings.

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import { useThemeContext } from "./context/theme-ctx";
 import Navbar from "./components/navbars/2";
 import HomePage from "./pages/home";
+import LoginPage from "./pages/login";
+import SignupPage from "./pages/signup";
 import ProjectsPage from "./pages/projects";
 import CommentsModal from "./components/modals/comments-modal";
 
@@ -15,10 +17,12 @@ export default function App(): ReactElement {
   return (
     <>
       <Navbar />
-
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/projects" element={<ProjectsPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
       </Routes>
 
       {/* Include all modal windows that are waiting for opening */}
