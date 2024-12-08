6ea91850154c801aaf138f33ed9b06409b796753
Amir Daneshmand <amirdanesh02@gmail.com>
2024-12-08 13:25:16 +0330

    Add Login and Signup forms.

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+import LoginForm from "../../components/forms/Login";
+
+export default function LoginPage(): ReactElement {
+  return (
+    <main id="page">
+      <LoginForm />
+    </main>
+  );
+}
---
+++ src/pages/signup/index.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+
+import SignupForm from "../../components/forms/Signup";
+
+export default function SignupPage(): ReactElement {
+  return (
+    <main id="page">
+      <SignupForm />
+    </main>
+  );
+}
